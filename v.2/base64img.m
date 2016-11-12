18bb8b2f21a3fbfd089c569ad7def891bf1656c3
Alex Rao <alexhrao@gmail.com>
2016-11-11 22:35:03 -0500

    Adding fig.Number, so that the figure can be consistently converted into a figure handle.

--- v.2/base64img.m
+++ v.2/base64img.m
@@ -39,7 +39,7 @@ end
 %the easiest way to get the figure's data in jpeg format is to save it as
 %temporary jpeg and clean it up afterwards
 file = [tempname '.jpg'];
-print(sprintf('-f%i',fig),'-djpeg',sprintf('-r%i', dpi), file);
+print(sprintf('-f%i',fig.Number),'-djpeg',sprintf('-r%i', dpi), file);
 base64string = base64file(file);
 delete(file);
 
@@ -47,7 +47,7 @@ if nargout == 0
     %if no output args, create a web page to display the image and show it
     s = sprintf(['<html><head><title>Matlab Figure: %i</title></head><body>'...
         '<img src="data:image/jpg;base64,%s"></body></html>'],...
-        fig, base64string);
+        fig.Number, base64string);
     web(['text://' s]);
 end 
 
