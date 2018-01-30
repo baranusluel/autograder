ffe1b7c11310678e6087037129e5be55c1e8230e
Wolfe, Triton Dev <triton.wolfe@gatech.edu>
2018-01-30 17:21:50 -0500

    finalized initial version

--- v.Triton/canvasParser.m
+++ v.Triton/canvasParser.m
@@ -1,15 +1,16 @@
-function parsedCanvasPath = canvasParser(canvasZipPath, canvasGradebookPath, hwName, resub)
+function parsedCanvasPath = canvasParser(canvasZipPath, canvasGradebookPath, hwName)
     
     % Get the fileparts of the zip path
-    [parentPath, name, ext] = fileparts(canvasZipPath);
+    [parentPath, ~, ~] = fileparts(canvasZipPath);
     
     % Create a temp dir and populate with submissions
     mkdir([parentPath 'canvasParserTemp']);
-    unzip(canvasZipPath, 'canvasParserTemp\submissions');
+    unzip(canvasZipPath, [parentPath 'canvasParserTemp\submissions']);
     
     students = containers.Map('KeyType','double','ValueType','any');
     
-    % Read the gradebook into students
+    % Read the gradebook into students and create individual student
+    % directories
     [~,~,gradebookRaw] = xlsread(canvasGradebookPath);
     dimvec = size(gradeboodRaw);
     for i = 3:dimvec(1)
@@ -23,11 +24,49 @@ function parsedCanvasPath = canvasParser(canvasZipPath, canvasGradebookPath, hwN
         students(id).lastName = lastName;
         students(id).gatechID = gtid;
         students(id).submissions = [];
-        studentPath = sprintf('canvasParserTemp\%s\%s, %s,(%d)', hwName, lastname, firstname, id);
+        students(id).path = sprintf([parentPath 'canvasParserTemp\%s\%s, %s,(%d)'], hwName, lastname, firstname, id);
         if ~exist(studentPath,'dir')
-            mkdir([studentPath '\Submission attachment(s)\'])
-            mkdir([studentPath '\Feedback attachment(s)\'])
+            mkdir([students(id).path '\Submission attachment(s)\'])
+            mkdir([student(id).path '\Feedback attachment(s)\'])
         end
     end
     
+    % parse the student submissions to respective directories.
+    allFiles = dir([parentPath 'canvasParserTemp\submissions\*.m']);
+    for file = allFiles'
+        tokens = strsplit(file.name, '_');
+        if length(tokens) ~= 4
+            if isnan(str2double(tokens{2}))
+                tokens{1} = [tokens{1} '_' tokens{2}];
+                tokens(2) = [];
+            end
+            if length(tokens) == 5
+                tokens{4} = [tokens{4} '_' tokens{5}];
+                tokens(5) = [];
+            end
+        end
+        id = str2double(tokens{2});
+        funcName = tokens{end};
+        if contains(funcName,'-')
+            nameTokens = strsplit(funcName, {'-', '.'});
+            if length(nameTokens) == 3
+                funcName = [nameTokens{1} nameTokens{3}];
+                funcVersion = str2double(nameTokens{2});
+            else
+                funcVersion = 0;
+            end
+        end
+        if ~isfield(students(id).submissions,nameTokens{1})
+            students(id).submissions.(nameTokens{1}) = funcVersion;
+            copyfile(fullfile(file.path, file.name), fullfile(students(id).path, funcName))
+        else
+            if student(id).submissions.(nameTokens{1}) < funcVersion
+                delete(fullfile(students(id).path, funcName));
+                students(id).submissions.(nameTokens{1}) = funcVersion;
+                copyfile(fullfile(file.path, file.name), fullfile(students(id).path, funcName))
+            end
+        end
+    end
+    parsedCanvasPath = fullfile(parentPath, hwName);
+    zip(parsedCanvasPath, hwName, fullfile(parentPath, 'canvasParserTemp'));
 end
