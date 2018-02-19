69d80a8460100856b3d5ce6abf0b4cb1d3b0c2d8
Triton Wolfe <triton.wolfe@gatech.edu>
2018-02-19 09:06:28 -0500

    add csv write capability

--- v.2/tsquare2canvasCSV.m
+++ v.2/tsquare2canvasCSV.m
@@ -1,6 +1,7 @@
 function tsquare2canvasCSV(tsquareCSV,canvasCSV,hwNumStr,resub)
     [~,~,tsquare] = xlsread(tsquareCSV);
     [~,~,canvas] = xlsread(canvasCSV);
+    canvas = canvas(:,1:22);
     canvasDimvec = size(canvas);
     if resub
         mask = contains(canvas(1,:),hwNumStr) & contains(canvas(1,:),'Resub');
@@ -13,5 +14,20 @@ function tsquare2canvasCSV(tsquareCSV,canvasCSV,hwNumStr,resub)
         grade = tsquare{t2mask,5};
         canvas{r,mask} = grade;
     end
-    xlswrite('grades1.xlsx',canvas);
+    parentPath = fileparts(canvasCSV);
+    fh = fopen([parentPath '\writtenGrades.csv'],'w');
+    for r = 1:canvasDimvec(1)
+        fprintf(fh,'"%s"',canvas{r,1});
+        for c = 2:canvasDimvec(2)
+            if isnan(canvas{r,c})
+                fprintf(fh,',');
+            elseif isnumeric(canvas{r,c})
+                fprintf(fh,',%.1f',canvas{r,c});
+            elseif ischar(canvas{r,c})
+                fprintf(fh,',%s',canvas{r,c});
+            end
+        end
+        fprintf(fh,'\n');
+    end
+    fclose(fh);
 end
