5ebfc41fe6838076e24442af6433e409f59ba6a3
Baran Usluel <busluel3@gatech.edu>
2018-03-03 22:25:40 -0500

    Add classdef, fields and method headers

--- modules/grader/Student.m
+++ modules/grader/Student.m
@@ -34,74 +34,96 @@
 %
 % **TBD**
 %
-%% gradeProblem: Grades the given problem and records the results
-%   
-% gradeProblem is used to evaluate the student code for a given problem and
-% records the results in the feedbacks field
-%
-% gradeProblem(PROBLEM) will take in a valid PROBLEM class, evaluates the 
-% student code, creates a Feedback instance for each TestCase, which in turn 
-% gets added to the feedbacks field.
-%
-%%% Remarks
-%
-% The feedback field should always be populated, even if submissions field is
-% empty
-%
-% If Matlab throws an exception, this function will catch the exception and
-% output it to the reason field in Feedback. If the reason field already
-% has content in it, the exception ID will be concatenated to the data
-% already found in reason.
-%
-%%% Exceptions
-%
-% TBD need to take care of infinite loops
-%
-%%% Unit Tests
-%
-% Given a student who had submitted all files:
-% The student code is evaluated and a Feedback instance is created.
-% The Feedback class will then be added to the feedbacks field.
-%
-% Given a student who had only partially submitted files:
-% The student code is evaluated normally for non-empty submissions
-% and Feedback instances are created. Empty submissions will
-% give appropritate score and reason values in the Feedback class. 
-% The Feedback classes will then be added to the feedbacks field.
-%
-% Given a student who had no submitted files:
-% Empty submissions will give appropritate score and reason values in the 
-% Feedback class.
-% The Feedback classes will then be added to the feedbacks field.
-%
-%% generateFeedback: Generate HTML feedback for student
-%
-% generateFeedback is used to create an HTML file containing the homework
-% feedback for a student.
-%
-% HTML = generateFeedback() will return a char vector HTML containing the
-% markup representing the student's feedback.
-%
-%%% Remarks
-%
-% Calls the generateFeedback method of the Feedback class, for each
-% Feedback in Student's feedbacks cell array, to get the HTML feedback for
-% individual TestCase's. Puts together a complete, personalized HTML
-% webpage for the student using this data and the name and id fields.
-%
-%%% Exceptions
-%
-% An AUTOGRADER:STUDENT:GENERATEFEEDBACK:MISSINGFEEDBACK exception will be
-% thrown if the feedbacks field of the Student is empty (i.e. if
-% gradeProblem wasn't invoked first).
-%
-%%% Unit Tests
-%
-% When invoked in a Student class containing a valid (non-empty)
-% feedbacks field, generateFeedback will generate an HTML page with:
-% - A header, containing the name of the homework as the title, and the
-% student's name and ID.
-% - A table with the student's points on each problem and total score.
-% - A section for each individual problem, where every test case and the
-% result (including points received, reason for losing points, visual
-% comparison of file outputs) is listed.
+classdef Student
+    properties (Access = public)
+        name;
+        id;
+        path;
+        submissions;
+        feedbacks;
+        isGraded;
+    end
+    methods (Access = public)
+        %% gradeProblem: Grades the given problem and records the results
+        %   
+        % gradeProblem is used to evaluate the student code for a given
+        % problem and records the results in the feedbacks field
+        %
+        % gradeProblem(PROBLEM) will take in a valid PROBLEM class,
+        % evaluates the student code, creates a Feedback instance for each
+        % TestCase, which in turn gets added to the feedbacks field.
+        %
+        %%% Remarks
+        %
+        % The feedback field should always be populated, even if
+        % submissions field is empty.
+        %
+        % If Matlab throws an exception, this function will catch the
+        % exception and output it to the reason field in Feedback. If the
+        % reason field already has content in it, the exception ID will be
+        % concatenated to the data already found in reason.
+        %
+        %%% Exceptions
+        %
+        % TBD need to take care of infinite loops
+        %
+        %%% Unit Tests
+        %
+        % Given a student who had submitted all files:
+        % The student code is evaluated and a Feedback instance is created.
+        % The Feedback class will then be added to the feedbacks field.
+        %
+        % Given a student who had only partially submitted files:
+        % The student code is evaluated normally for non-empty submissions
+        % and Feedback instances are created. Empty submissions will
+        % give appropritate score and reason values in the Feedback class. 
+        % The Feedback classes will then be added to the feedbacks field.
+        %
+        % Given a student who had no submitted files:
+        % Empty submissions will give appropritate score and reason values
+        % in the Feedback class.
+        % The Feedback classes will then be added to the feedbacks field.
+        %
+        function gradeProblem(problem)
+            
+        end
+        
+        %% generateFeedback: Generate HTML feedback for student
+        %
+        % generateFeedback is used to create an HTML file containing the
+        % homework feedback for a student.
+        %
+        % HTML = generateFeedback() will return a char vector HTML
+        % containing the markup representing the student's feedback.
+        %
+        %%% Remarks
+        %
+        % Calls the generateFeedback method of the Feedback class, for each
+        % Feedback in Student's feedbacks cell array, to get the HTML
+        % feedback for individual TestCase's. Puts together a complete,
+        % personalized HTML webpage for the student using this data and the
+        % name and id fields.
+        %
+        %%% Exceptions
+        %
+        % An AUTOGRADER:STUDENT:GENERATEFEEDBACK:MISSINGFEEDBACK exception
+        % will be thrown if the feedbacks field of the Student is empty
+        % (i.e. if gradeProblem wasn't invoked first).
+        %
+        %%% Unit Tests
+        %
+        % When invoked in a Student class containing a valid (non-empty)
+        % feedbacks field, generateFeedback will generate an HTML page
+        % with:
+        % - A header, containing the name of the homework as the title, and
+        % the student's name and ID.
+        % - A table with the student's points on each problem and total
+        % score.
+        % - A section for each individual problem, where every test case
+        % and the result (including points received, reason for losing
+        % points, visual comparison of file outputs) is listed.
+        function html = generateFeedback()
+            
+        end
+    end
+end
