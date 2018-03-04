05e248e2f1c33222d214e0b4ad2f8c7a3cf6c9a2
Tobin Abraham <tabraham7@gatech.edu>
2018-03-03 22:04:11 -0500

    Add student.gradeProblem spec

--- modules/grader/Student.m
+++ modules/grader/Student.m
@@ -2,8 +2,9 @@
 %
 % Represents a single student in the grader.
 % 
-% Holds student's relevant information (identification, submissions,
-% feedback, etc.) as fields.
+% Holds student's relevant information (identification, submissions, 
+%feedback, etc.) 
+%as fields.
 %
 % Has methods to grade student's submissions and generate Feedback.
 %
@@ -34,6 +35,44 @@
 % **TODO**
 %
 
-%% gradeProblem
-
+%% gradeProblem: Grades the given problem and records the results
+%   
+% gradeProblem is used to evaluate the student code for a given problem and
+% records the results in the feedbacks field
+%
+% gradeProblem(PROBLEM) will take in a valid PROBLEM class, evaluates the 
+% student code, creates a Feedback instance for each TestCase, which in turn 
+% gets added to the feedbacks field.
+%
+%%% Remarks
+%
+% The feedback field should always be populated, even if submissions field is
+% empty
+%
+% If Matlab throws an exception, this function will catch the exception and
+% output it to the reason field in Feedback. If the reason field already
+% has content in it, the exception ID will be concatenated to the data
+% already found in reason.
+%
+%%% Exceptions
+%
+% TBD need to take care of infinite loops
+%
+%%% Unit Tests
+%
+% Given a student who had submitted all files:
+% The student code is evaluated and a Feedback instance is created.
+% The Feedback class will then be added to the feedbacks field.
+%
+% Given a student who had only partially submitted files:
+% The student code is evaluated normally for non-empty submissions
+% and Feedback instances are created. Empty submissions will
+% give appropritate score and reason values in the Feedback class. 
+% The Feedback classes will then be added to the feedbacks field.
+%
+% Given a student who had no submitted files:
+% Empty submissions will give appropritate score and reason values in the 
+% Feedback class.
+% The Feedback classes will then be added to the feedbacks field.
+%
 %% generateFeedback
