2634dd8f190fddbe6fe5aa51297b6e38c3a1d80f
Margaret A Nugent <mnugent8@gatech.edu>
2018-03-07 16:55:06 -0500

    Create initial generateStudents specification

--- modules/grader/TestCase.m
+++ modules/grader/TestCase.m
@@ -6,17 +6,25 @@
 %
 % * call: The complete function call 
 % (i.e., |[out1, out2] = myFunction(in1, in2);|)
+%
 % * initializer: The complete function call for a function to be run 
 % immediately before testing the student's code.
+%
 % * points: The points possible for this specific test case
+%
 % * inputs: A structure where the field name is the name of the variable, 
 % and the field value is the value of the variable
+%
 % * supportingFiles: A string array of complete file paths that will need 
 % to be copied to the student's directory
+%
 % * outputs: A structure where the field name is the name of the output, 
 % and the field value is the value of the output
+%
 % * images: A |File| array that represents the images produced as outputs
+%
 % * files: A |File| array that represents all the files produced as outputs
+%
 % * plots: A |Plot| array that represents the plots generated
 %
 %%% Methods
---
+++ modules/grader/generateStudents.m
@@ -0,0 +1,28 @@
+%% generateStudents: Generate Student array
+%
+% generateStudents takes in a path to the autograder's working folder, and
+% converts the student data to a Student array.
+%
+% [Student[] students] = generateStudents(string path);
+%
+%%% Remarks
+%
+% (describe the types of inputs and outputs)
+% input will be the full path to the student's folder (always a string or 
+% char?)
+% output will be student array?
+%
+%%% Exceptions
+%
+% ?
+%
+%%% Unit Tests
+% ideas: 1) path is valid and folder is there correctly
+%        2) if directory doesn't exist
+%        3) if path is invalid
+%
+% format:
+%    inputs
+%    function call
+%    
+%    output
