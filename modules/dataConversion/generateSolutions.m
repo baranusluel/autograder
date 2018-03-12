00f8381465adcc8ccbf893272c94633a5d43ea4c
Alexander Rao <arao81@gatech.edu>
2018-03-12 10:37:46 -0400

    Add JSON example for generateSolutions

--- modules/dataConversion/generateSolutions.m
+++ modules/dataConversion/generateSolutions.m
@@ -9,26 +9,119 @@
 % 
 %%% Remarks
 % 
-%
+% Any exceptions thrown by the Problem class or sub classes will not be 
+% caught by generateSolutions, and will instead by propogated forward.
+% These errors are considered mostly fatal.
 % 
+% The JSON format is strictly enforced. For more information on what 
+% this format should look like, see the central documentation. For 
+% convenience, an example is shown below.
+%
+%   {
+%     "Problems": [
+%       {
+%         "name": "ExampleProblem1",
+%         "banned": [
+%           "fopen",
+%           "fclose",
+%           "fseek",
+%           "frewind"
+%         ],
+%         "TestCases": [
+%           {
+%             "call": "[out1, out2] = myFun(in1, in2);",
+%             "initializer": "",
+%             "points": 3,
+%             "inputs": {
+%               "in1": 5,
+%               "in2": true
+%             },
+%             "supportingFiles": [
+%               "myFile.txt",
+%               "myInputImage.png"
+%             ]
+%           },
+%           {
+%             "call": "[out1, out2] = myFun(in1, in2);",
+%             "initializer": "in2 = supportFunction__;",
+%             "points": 3,
+%             "inputs": {
+%               "in1": 5
+%             },
+%             "supportingFiles": [
+%               "myFile.txt",
+%               "myInputImage.png",
+%               "supportFunction__.m"
+%             ]
+%           }
+%         ]
+%       },
+%       {
+%         "name": "ExampleProblem2",
+%         "banned": [
+%           "size",
+%           "parpool"
+%         ],
+%         "TestCases": [
+%           {
+%             "call": "[out1, out2] = myFun(in1, in2);",
+%             "initializer": "",
+%             "points": 3,
+%             "inputs": {
+%               "in1": 5,
+%               "in2": true
+%             },
+%             "supportingFiles": [
+%               "myFile.txt",
+%               "myInputImage.png"
+%             ]
+%           },
+%           {
+%             "call": "[out1, out2] = myFun(in1, in2);",
+%             "initializer": "in2 = supportFunction__;",
+%             "points": 3,
+%             "inputs": {
+%               "in1": 5
+%             },
+%             "supportingFiles": [
+%               "myFile.txt",
+%               "myInputImage.png",
+%               "supportFunction__.m"
+%             ]
+%           }
+%         ]
+%       }
+%    ]
+%  }
+%
 %%% Exceptions
 % 
-% generateSolutions throws exception ?? if the input path is invalid
-% generateSolutions throws exception ?? if a necessary file is missing in
-% the given directory
-% 
+% generateSolutions throws exception 
+% AUTOGRADER:GENERATESOLUTIONS:INVALIDPATH if the input path is invalid
+% or if necessary file is missing in the given directory
 % 
 %%% Unit Tests
 % 
-% 1) Valid Case --> Returns Problem Array with all field correctly filled
-% for each problem
+%   PATH = 'C:\Users\...\Soln.zip'; % Valid Solutions Archive
+%   PROBLEMS = generateSolutions(PATH);
 %
-% 2) If the path is invalid --> throws an exception
-% 
-% 3) If the solution file errors --> Exception thrown in TestCase constructor
-% not caught in generateSolutions
-% 
-% 4) If there is a missing file (solution file, supporting file, JSON,etc)
-% --> Throws an exception
+%   PROBLEMS -> Valid Problem Array
+%
+%   PATH = ''; % Inavlid Path
+%   PROBLEMS = generateSolutions(PATH);
+%
+%   Threw INVALIDPATH exception
+%
+%   PATH = 'C:\Users\...\Soln.zip'; % Valid path, invalid solutions!
+%   PROBLEMS = generateSolutions(PATH);
+%
+%   TestCase Threw exception <SolnException>
+%
+%   PATH = 'C:\Users\...\Soln.zip'; % Valid path, but incomplete archive
+%   PROBLEMS = generateSolutions(PATH);
+%
+%   Threw INVALIDPATH exception
+%
+function problems = generateSolutions(path)
 
-
+end
