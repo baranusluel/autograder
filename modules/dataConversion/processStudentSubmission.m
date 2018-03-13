c0bbb6f1be22349928c1b3ceb717bc48f00efc1c
Wolfe, Triton Dev <triton.wolfe@gatech.edu>
2018-03-13 08:46:21 -0400

    Expand processStudentSubmission documentation, adding more explicit unit tests and description

--- modules/dataConversion/processStudentSubmission.m
+++ modules/dataConversion/processStudentSubmission.m
@@ -3,18 +3,17 @@
 % processStudentSubmission will unpack a student's submissions into their same 
 % folder.
 %
-% processStudentSubmission(P) will try and unpack
-% the data located at the given pathway into a single folder. P is a 
-% string or character vector that represents the path to the student's 
-% folder.
+% processStudentSubmission(P) will remove all non .m and all non
+% .zip files in path P. If a .zip exists, it will unzip the file
+% once and remove all non .m files.
 %
 %%% Remarks
 %
-% If the student submitted each file seperately, this function will do
-% nothing.
+% If the student submitted each .m file separately, this function will not
+% change the folder at all.
 %
-% processStudentSubmission won't _recursively_ unzip archives. In other words,
-% suppose the student submits a ZIP archive that has, inside of it, _another_ 
+% processStudentSubmission won't recursively unzip archives. In other words,
+% suppose the student submits a ZIP archive that has, inside of it, another 
 % ZIP archive. That second archive will not be unzipped!
 %
 % In the event of a name collision (i.e., suppose the ZIP archive has a file
@@ -22,25 +21,102 @@
 % Note that in the case of having multiple archives present, there is no 
 % guarantee as to which file will survive if both archives have the same file.
 %
+% In the event of a name collision within given zip archives, the files 
+% contained in the zip that appears first from the dir command will 
+% supercede all subsequent files.
+%
 %%% Exceptions
 %
-% An AUTOGRADER:ISEMPTY:INVALIDPATH exception will be thrown if 
-% the pathway is invalid
+% An AUTOGRADER:PROCESSSTUDENTSUBMISSION:INVALIDPATH exception will be 
+% thrown if there is no path.
+%
+% An AUTOGRADER:PROCESSSTUDENTSUBMISSION:INVALIDDIR exception will be 
+% thrown if the path given is not a directory.
 %
 %%% Unit Tests
 %
-%   Given there is only files found on the pathway, the files will be moved
-%   to the student's labeled folder.
+% 1)
+% Folder Containing:
+%   Several .m files
 %
-%   Given a pathway with one or more ZIP archive, the function will open the ZIP
-%   archive and move its contents to the student's folder.
+% Inputs:
+%   Path to the Folder
 %
-%   Given both ZIP archives and files found on the given pathway, the files
-%   found will be moved to the student's labeled folder and then the ZIP
-%   archives will be opened and their contents moved to the student's
-%   folder.
+% Runtime:
+%   None
 %
-%   P = ''; % Invalid Path
-%   processStudentSubmissions(P);
+% Outputs:
+%   None
 %
-%   Threw INVALIDPATH exception
+% Folder Containing:
+%   Several .m files
+%
+% 2)
+% Folder Containing:
+%   Several .m files
+%   Several .png files
+%   Several .txt files
+%
+% Inputs:
+%   Path to the Folder
+%
+% Runtime:
+%   None
+%
+% Outputs:
+%   None
+%
+% Folder Containing:
+%   Several .m files
+%
+% 3)
+% Folder Containing:
+%   2 zip archives containing the same files (a.zip and b.zip)
+%
+% Inputs:
+%   Path to the Folder
+%
+% Runtime:
+%   None
+%
+% Outputs:
+%   None
+%
+% Folder Containing:
+%   Several .m files (from the a.zip)
+%
+% 4)
+% Folder Containing:
+%   Several .m files
+%   A zip archive (with no name collisions) containing several .m files,
+%    several .png files, and several .txt files
+%
+% Inputs:
+%   Path to the Folder
+%
+% Runtime:
+%   None
+%
+% Outputs:
+%   None
+%
+% Folder Containing:
+%   Several more .m files (from the the zip and original)
+% 
+% 5)
+% Folder Containing:
+%   Nothing
+%
+% Inputs:
+%   Path to the Folder
+%
+% Runtime:
+%   None
+%
+% Outputs:
+%   None
+%
+% Folder Containing:
+%   Nothing
+%
+% 6-7) Test Exceptions
