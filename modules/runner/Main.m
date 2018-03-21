3053e4f427335f660893c3eced3a5a075f14d832
Alex Rao <arao81@gatech.edu>
2018-03-21 11:55:16 -0400

    Add basic skeleton for Main method

--- modules/runner/Main.m
+++ modules/runner/Main.m
@@ -2,12 +2,166 @@
 %
 % Main runs the autograder completely, cleaning up after itself and
 % generally ensuring nothing is changed in the host environment
+%
+% Main() Will prompt the user for the student ZIP file, the solution ZIP
+% file, and will proceed to run the autograder.
+%
+% Main(S, N) will run the autograder for the given student and solution zip
+% archive paths. If the paths are invalid, it will error.
+%
+%%% Remarks
+%
+% Main is the entry point for the Autograder. Main will handle any
+% exceptions, and will always clean up after itself - the user's folder
+% will not be changed!
+%
+% Documentation is available. Please go the the autograder's website, at
+%
+% <https://github.gatech.edu/pages/CS1371/autograder Documentation>
+%
+%%% Exceptions
+%
+% An AUTOGRADER:INVALIDPATH exception will be thrown if either input paths
+% are invalid.
+%
+%%% Unit Tests
+%
+%   S = ''; % invalid path
+%   N = ''; % invalid path
+%   Main(S, N);
+%
+%   Threw AUTOGRADER:INVALIDPATH exception
+%
+function Main(students, solutions)
+    % Implementation Notes:
+    % 
+    % Main() will provide initial checking, and set up all necessary
+    % processes and environment settings:
+    %
+    % *Startup*
+    % 
+    % * Start the Parallel Pool
+    % * Create the SENTINEL file; assign to the File class
+    % * Apply the factory default path, and remove the user's MATLAB path
+    % * Create a temporary folder, and copy the student and solution zip
+    % files there
+    % * Ensure figures don't become visible
+    %
+    % *Cleanup*
+    %
+    % * Shut down the parallel pool
+    % * Close and delete the SENTINEL file
+    % * Reapply the path of the original user
+    % * Copy any necessary data back from temp folder (?)
+    % * Reapply user's settings for figures
+    
+    
+    % Start up parallel pool
+    if isempty(gcp)
+        parpool(2);
+    end
+    
+    % Get temporary directory
+    settings.workingDir = [tempname filesep];
+    mkdir(settings.workingDir);
+    settings.userDir = cd(settings.workingDir);
+    % Create SENTINEL file
+    fid = fopen('SENTINEL.lock', 'wt');
+    fwrite(fid, 'SENTINEL');
+    fclose(fid);
+    
+    % Copy zip files
+    % rename appropriately (students -> students, solutions -> solutions)
+    copyfile(students, [workingDir 'students.zip']);
+    copyfile(solutions, [workingDir 'solutions.zip']);
+    studPath = [workingDir 'students.zip'];
+    solnPath = [workingDir 'solutions.zip'];
+    % Remove user's PATH, instate factory default instead:
+    settings.userPath = path();
+    restoredefaultpath();
+    userpath('reset');
+    userpath('clear');
+    
+    % Make sure figure's don't show
+    settings.figures = get(0, 'DefaultFigureVisible');
+    set(0, 'DefaultFigureVisible', 'off');
+    
+    % Set on cleanup
+    cleaner = onCleanup(@() cleanup(settings));
+    
+    % Generate solutions
+    try
+        solutions = generateSolutions(solnPath);
+    catch e
+        % Display to user that we failed
+        alert('Problem generation failed. Error %s: %s', e.identifier, ...
+            e.message);
+        return;
+    end
+    
+    % Generate students
+    try
+        students = generateStudents(studPath);
+    catch e
+        alert('Student generation failed. Error %s: %s', e.identifier, ...
+            e.message);
+        return;
+    end
+    
+    % Grade students
+    %
+    % There are a couple of ways to do this. I've listed them below:
+    %
+    % * For each student, for each problem, grade the problem and provide
+    % feedback
+    % * For each student, for each problem, grade the problem. For each
+    % student, generate the feedback
+    % * For each problem for each student, grade the problem. For each
+    % student, generate the feedback
+    %
+    % I am going to choose option one. I think this will have a better
+    % performance measure, but that's just a gut decision. If anyone has
+    % any better ideas, I'm all ears!
+    %
+    % Also, if we grade and provide feedback all at once, then if either
+    % step will fail, it will fail fast (at the first student) rather than
+    % after all grading has been done.
+    for s = 1:numel(students)
+        student = students(s);
+        for p = 1:numel(solutions)
+            problem = problems(p);
+            student.gradeProblem(problem);
+            student.generateFeedback();
+        end
+    end
+end
 
-% Implementation notes:
-% Start the parpool if isempty(gcp) parpool;
-% Create and assign the SENTINEL file of File.m
-% factory default path: origPath = path(); restoredefaultpath();
-% path(origPath, '');
-% After running, close the parallel pool (delete(gcp('nocreate'));
-% After running, delete the sentinel file (fclose('all'),
-% delete(File.SENTINEL);)
+function alert(msg, params)
+    if ~exist('params', 'var') || isempty(params)
+        fprintf(2, '%s\n', msg);
+    else
+        fprintf(2, [msg '\n'], params{:});
+    end
+end
+
+function cleanup(settings)
+    % Cleanup
+    % Delete the parallel pool
+    if ~isempty(gcp('nocreate'))
+        delete(gcp);
+    end
+    
+    % Restore user's path
+    path(settings.userPath, '');
+    
+    % Copy over any files (?)
+    
+    % cd to user's dir
+    cd(settings.userDir);
+    
+    % Delete our working directory
+    rmdir(settings.workingDir, 's');
+    
+    % Restore figure settings
+    set(0, 'DefaultFigureVisible', settings.figures);
+end
