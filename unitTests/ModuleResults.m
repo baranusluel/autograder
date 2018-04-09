a3d1c85320d3079154b87ba6359c973613fcddb3
Alexander Rao <arao81@gatech.edu>
2018-04-09 12:01:59 -0400

    Add spec for ModuleResults

---
+++ unitTests/ModuleResults.m
@@ -0,0 +1,18 @@
+%% ModuleResults: Run and record all tests for this Module
+%
+% ModuleResults is responsible for running _all_ the unit tests for this module
+%
+%%% Fields
+%
+% * path: The path to this module
+% * name: The name of this module
+% * passed: Whether or not this module passed inspection
+%
+%%% Methods
+%
+% * ModuleResults
+% * generateHtml
+%
+%%% Remarks
+%
+% This is used by the builder to test an entire module
