70320b3a3737bf9e28b838af1427bd867f596055
Alexander Rao <arao81@gatech.edu>
2018-04-13 09:03:06 -0400

    Add Spec for img2base64

---
+++ modules/dataConversion/img2base64.m
@@ -0,0 +1,38 @@
+%% img2base64: Convert an image to a Base 64 string
+%
+% img2base64 will convert an image to a base64 string with uri tags as
+% well.
+%
+% B = img2base64(I) will convert I to base64 string B using PNG
+% compression formats. The string B will include the |data| and |base64|
+% attributes, which means it can be injected directly inside of HTML.
+%
+% B = img2base64(I, F) will use the format in F to encode I as a base 64
+% string B.
+%
+%%% Remarks
+%
+% This is used extensively wherever we need to show an image, since
+% including an image as an attachment is usually frowned upon.
+%
+%%% Exceptions
+%
+% If the format is not recognized, then this will throw an 
+% AUTOGRADER:img2base64:invalidFormat exception.
+%
+%%% Unit Tests
+%
+%   I = imread(...); % valid image
+%   B = img2base64(I);
+%
+%   B -> 'data:image/png;base64,STRING...';
+%
+%   I = imread(...); % valid iamge
+%   B = img2base64(I, 'bmp');
+%
+%   B -> 'data:image/bmp;base64,STRING...';
+%
+%   I = imread(...); % valid image
+%   B = img2base64(I, 'unknownType');
+%
+%   Threw invalidFormat exception
