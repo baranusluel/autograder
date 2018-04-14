971e615d63a2d9a905d28dc8c84394def82b37c4
Alexander Rao <arao81@gatech.edu>
2018-04-14 15:56:08 -0400

    Add initial spec for downloadFromCanvas

---
+++ modules/networking/downloadFromCanvas.m
@@ -0,0 +1,8 @@
+%% downloadFromCanvas: Download the ZIP archive from Canvas
+%
+% downloadFromCanvas will download the given assignment to be parsed by the
+% autograder
+%
+% downloadFromCanvas(C, A, P) will use the course ID in C, the assignment
+% ID in A, and the path in P to download and save the homework submission
+% as a Zip archive in path P.
