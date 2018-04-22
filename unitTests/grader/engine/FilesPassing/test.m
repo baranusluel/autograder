cb86f8107556a65a34ff53dafbd1078275465b78
Alexander Rao <arao81@gatech.edu>
2018-04-22 15:13:29 -0400

    Augment engine to ban the load function; add start to FilesPassing test

---
+++ unitTests/grader/engine/FilesPassing/test.m
@@ -0,0 +1,6 @@
+%% Passing Files
+%
+% This unit test has a solution which produces two files, and a student
+% that also produces the exact same files.
+function [passed, msg] = test()
+    % set up test case
