65ff04009b2a73beff5651a56d9662a002b4b30c
Alexander Rao <arao81@gatech.edu>
2018-04-27 15:31:11 -0400

    Address issue #72 for generateStudents spec

--- modules/dataConversion/generateStudents.m
+++ modules/dataConversion/generateStudents.m
@@ -2,14 +2,14 @@
 %
 % generateStudents turns a set of Student Folders into a vector of Students
 %
-% [S] = generateStudents(P) will convert all the student folders found in P
+% S = generateStudents(P, B) will convert all the student folders found in P
 % into Students, and will return a vector in alphabetical order (case insensitive)
 % of the Students, where alphabetical order is based on their ID.
+% Additionally, it will attempt to update progress bar B.
 %
 %%% Remarks
 %
-% generateStudents will always expect a correctly-structured archive, and also
-% always assumes that all students have been previously unpacked.
+% generateStudents will always expect a correctly-structured path.
 %
 %%% Exceptions
 %
@@ -21,18 +21,20 @@
 %
 %%% Unit Tests
 %
+%   For all cases, assume B is a valid uiprogressdlg.
+%
 %   P = 'C:\Users\...\students.zip'; % Valid path to student folders
-%   S = generateStudents(P);
+%   S = generateStudents(P, B);
 %
 %   S -> A vector of students in alphabetical order, by their GT Username
 %
 %   P = ''; % Invalid Path
-%   S = generateStudents(P);
+%   S = generateStudents(P, B);
 %
 %   Threw invalidPath exception
 %
 %   P = ''; % Valid path, but no student folders are found
-%   S = generateStudents(P);
+%   S = generateStudents(P, B);
 %
 %   Threw foldersNotFound exception
 %
@@ -46,56 +48,56 @@
 % And then work your way down to 1 from there.
 
 function students = generateStudents(path, progress)
-
-progress.Message = 'Generating Students';
-progress.Indeterminate = 'on';
-if ~isfolder(path) % if path doesn't lead to existing folder, exception
-    msgID = 'AUTOGRADER:generateStudents:invalidPath';
-    msgtext = 'path is invalid';
-    ME = MException(msgID, msgtext);
-    throw(ME);
-else % if path leads to folder
-    studs = dir(path);
-    studs(strncmp({studs.name}, '.', 1)) = []; % filter out '.' and '..'
-    studs(~[studs.isdir]) = []; % filter out any misc files
-    if isempty(studs) % if there are no student folders, exception
-        msgID = 'AUTOGRADER:generateStudents:foldersNotFound';
-        msgtext = 'no student folders were found';
+    progress.Message = 'Generating Students';
+    progress.Indeterminate = 'on';
+    if ~isfolder(path) % if path doesn't lead to existing folder, exception
+        msgID = 'AUTOGRADER:generateStudents:invalidPath';
+        msgtext = 'path is invalid';
         ME = MException(msgID, msgtext);
         throw(ME);
-    else
-        % make vector of Students
-        CSV_NAME = 'info.csv'; % magic variable for csv filename
-        FULLNAME_COL = 1; % magic number for col with full names
-        GT_USERNAME_COL = 2; % magic number for col with usernames
-        fid = fopen([path filesep CSV_NAME], 'rt');
-        raw = textscan(fid, '%q%q', 'Delimiter', ',');
-        fclose(fid);
-        studentNames = raw{FULLNAME_COL};
-        users = raw{GT_USERNAME_COL};
-        progress.Indeterminate = 'off';
-        progress.Value = 0;
-        for i = length(studs):-1:1
-            % Student constructor takes in path to individual student
-            % folder and student's full name
-            studentPath = fullfile(studs(i).folder, studs(i).name);
-            studentName = studentNames{strcmp(users, studs(i).name)};
-            workers(i) = parfeval(@createStudent, 1, studentPath, studentName);
+    else % if path leads to folder
+        studs = dir(path);
+        studs(strncmp({studs.name}, '.', 1)) = []; % filter out '.' and '..'
+        studs(~[studs.isdir]) = []; % filter out any misc files
+        if isempty(studs) % if there are no student folders, exception
+            msgID = 'AUTOGRADER:generateStudents:foldersNotFound';
+            msgtext = 'no student folders were found';
+            ME = MException(msgID, msgtext);
+            throw(ME);
+        else
+            % make vector of Students
+            CSV_NAME = 'info.csv'; % magic variable for csv filename
+            FULLNAME_COL = 1; % magic number for col with full names
+            GT_USERNAME_COL = 2; % magic number for col with usernames
+            fid = fopen([path filesep CSV_NAME], 'rt');
+            raw = textscan(fid, '%q%q', 'Delimiter', ',');
+            fclose(fid);
+            studentNames = raw{FULLNAME_COL};
+            users = raw{GT_USERNAME_COL};
+            progress.Indeterminate = 'off';
+            progress.Value = 0;
+            for i = length(studs):-1:1
+                % Student constructor takes in path to individual student
+                % folder and student's full name
+                studentPath = fullfile(studs(i).folder, studs(i).name);
+                studentName = studentNames{strcmp(users, studs(i).name)};
+                workers(i) = parfeval(@createStudent, 1, studentPath, studentName);
+            end
+            students = cell(1, numel(studentNames));
+            while ~all([workers.Read])
+                [idx, stud] = fetchNext(workers);
+                students{idx} = stud;
+                progress.Value = min([progress.Value + 1/length(studs), 1]);
+            end
+            students = [students{:}];
+            % alphabetize vector of Students based on GT username
+            [~, idx] = sort({students.name});
+            students = students(idx);
         end
-        students = cell(1, numel(studentNames));
-        while ~all([workers.Read])
-            [idx, stud] = fetchNext(workers);
-            students{idx} = stud;
-            progress.Value = min([progress.Value + 1/length(studs), 1]);
-        end
-        students = [students{:}];
-        % alphabetize vector of Students based on GT username
-        [~, idx] = sort({students.name});
-        students = students(idx);
     end
 end
 
 function student = createStudent(path, name)
     processStudentSubmission(path);
-    student = Student(path, name)
+    student = Student(path, name);
 end
