22c4dd4ace4dd98aa8785a48a7b4b0fce0e76127
Alexander Rao <arao81@gatech.edu>
2018-04-27 15:49:14 -0400

    Write spec for uploadToServer

--- modules/networking/uploadToServer.m
+++ modules/networking/uploadToServer.m
@@ -1,3 +1,30 @@
-%% uploadToWebsite: Upload the student's submission files to the Server
+%% uploadToServer: Upload the student's submission files to the Server
 %
+% uploadToServer is responsible for uploading files to the CS 1371 Server
 %
+% uploadToServer(S, H, P, U, W, B) will upload the files for students in 
+% array S to the host H, at port P, using the username U and the password
+% W. Additionally, it will update the progress bar B.
+%
+%%% Remarks
+%
+% This method is used to upload student files to the CS 1371 website, so
+% that the students can view them.
+%
+%%% Exceptions
+%
+% This method, like all other networking methods, will throw an
+% AUTOGRADER:networking:connectionError exception if interrupted.
+%
+%%% Unit Tests
+%
+%   S = Student(); % valid student array
+%   H = 'cs1371.gatech.edu'; % valid host
+%   P = 22; % valid port
+%   U = 'autograder'; % valid username
+%   W = 'password'; % valid password
+%   B = uiprogressdlg;
+%   uploadToServer(S, H, P, U, W, B);
+%
+%   Student files are correctly uploaded
+function uploadToServer(students, host, port, username, password, progress)
