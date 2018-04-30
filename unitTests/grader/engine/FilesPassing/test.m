71bcf9fd88747420a336f88c2a92e9b395ccfd7f
Alexander Rao <arao81@gatech.edu>
2018-04-30 12:54:29 -0400

    Add Multiple Files test for Engine

--- unitTests/grader/engine/FilesPassing/test.m
+++
@@ -1,6 +0,0 @@
-%% Passing Files
-%
-% This unit test has a solution which produces two files, and a student
-% that also produces the exact same files.
-function [passed, msg] = test()
-    % set up test case
