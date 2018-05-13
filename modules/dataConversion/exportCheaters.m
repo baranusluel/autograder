db8eee0ee1af540c63b320c3abbae9147ea918c4
Alexander Rao <arao81@gatech.edu>
2018-05-13 01:22:33 -0500

    Finish exporter for raw code files

--- modules/dataConversion/exportCheaters.m
+++ modules/dataConversion/exportCheaters.m
@@ -3,12 +3,9 @@
 % exportCheaters is used to export all the cheaters to a nice,
 % human-readable format.
 %
-% exportCheaters(S, C, P) will export the students in S with scores C to
-% path P. If P does not exist, it will be created. An assumed threshold of
-% 0.95 will be used
-%
-% exportCheaters(S, C, P, T) will do the same as above, but use the
-% threshold T, where T lies between 0 and 1, inclusive.
+% exportCheaters(S, C, P, D, G) will use the student array S, cheater array
+% C, problem name array P, directory path D, and progress bar G to export
+% cheater code files to their respective folders.
 %
 %%% Remarks
 %
@@ -16,23 +13,39 @@
 %%% Exceptions
 %
 % This function will never throw an exception
-function exportCheaters(students, problems, scores, path, threshold)
-    if nargin < 4
-        threshold = 0.95;
-    end
+function exportCheaters(students, cheaters, problems, path, progress)
     if ~isfolder(path)
         mkdir(path);
     end
     
     orig = cd(path);
-    
+    progress.Message = 'Preparing to export';
+    progress.Value = 0;
     % for each student, get worker
     for s = numel(students):-1:1
-        sPath = [pwd filesep students(s).name];
-        % for each problem (except ABCs), if we have cheaters that meet
-        % threshold, engage.
-        
-        
+        sPath = [pwd filesep students(s).id];
+        % construct names & paths
+        for p = numel(problems):-1:1
+            if isempty(cheaters{s}{p})
+                names{p} = {};
+                paths{p} = {};
+            else
+                names{p} = [{cheaters{s}{p}.id}, {students(s).id}];
+                paths{p} = [cellfun(@(pSet)(pSet(p)), {cheaters{s}{p}.problemPaths}), students(s).problemPaths(p)];
+            end
+        end
+        if ~all(cellfun(@isempty, names))
+            workers(s) = parfeval(@exportStudent, 0, sPath, problems, names, paths);
+        end
+        progress.Value = min([progress.Value + 1/numel(students), 1]);
+    end
+    
+    workers([workers.ID] == -1) = [];
+    progress.Message = 'Exporting Student Code';
+    progress.Value = 0;
+    while ~all([workers.Read])
+        workers.fetchNext();
+        progress.Value = min([progress.Value + 1/numel(workers), 1]);
     end
     cd(orig);
     
@@ -43,13 +56,15 @@ function exportStudent(studentPath, problems, names, paths)
     mkdir(studentPath);
     orig = cd(studentPath);
     for p = 1:numel(problems)
-        mkdir(problems{p});
-        cd(problems{p});
-        % for each file, copy it with name of student
-        for f = 1:numel(paths{p})
-            copyfile(paths{p}{f}, names{p}{f});
+        if ~isempty(names{p})
+            mkdir(problems{p});
+            cd(problems{p});
+            % for each file, copy it with name of student
+            for f = 1:numel(paths{p})
+                copyfile(paths{p}{f}, [names{p}{f} '.m']);
+            end
+            cd('..');
         end
-        cd('..');
     end
     cd(orig);
 end
