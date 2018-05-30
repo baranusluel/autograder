de71b959ac27f9cb22c8742c0408339f587b3241
Alexander Rao <arao81@gatech.edu>
2018-05-30 14:39:17 -0400

    Add initial spec for textMessenger

---
+++ modules/networking/textMessenger.m
@@ -0,0 +1,11 @@
+%% textMessenger: Send a text to a phone number
+%
+% textMessenger(N, S, M) will send message M with subject S to phone number
+% N.
+%
+%%% Remarks
+%
+% This is based on the numverifier API, which allows us to programmatically
+% determine the carrier for a phone number.
+%
+%
