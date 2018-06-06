2585b817e65c61564987cc05e9ccf81e36304498
Alexander Rao <arao81@gatech.edu>
2018-06-06 07:00:21 -0400

    Add start of implementation for new diff algorithm

--- modules/grader/fileDiff.m
+++ modules/grader/fileDiff.m
@@ -14,5 +14,20 @@
 % The goal of this function is to create modular HTML markup that utilized
 % Bootstrap's grid to return meaningul and, ultimately, responsive HTML.
 function html = fileDiff(txt1, txt2, isBoilerplate)
+    javaaddpath([fileparts(mfilename('fullpath')) filesep 'diff' filesep]);
+    cleaner = onCleanup(@()(...
+        javarmpath([fileparts(mfilename('fullpath')) filesep 'diff' filesep])));
+    
+    dmp = diff_match_patch();
+    diffs = dmp.diff(txt1, txt2);
+    
+    % for each diff of left side, print accordingly?
+    
+end
 
+function line = sanitize(line)
+    line = strrep(line, '&', '&amp;');
+    line = strrep(line, '<', '&lt;');
+    line = strrep(line, '>', '&gt;');
+    line = strrep(line, newline, '&para;<br>');
 end
