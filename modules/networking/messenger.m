58525778718216a14bda55765d2b6c8a26fedaac
Alexander Rao <arao81@gatech.edu>
2018-06-06 10:51:39 -0400

    Add initial spec for messenger

---
+++ modules/networking/messenger.m
@@ -0,0 +1,12 @@
+%% messenger: Send messages on all predefined channels
+%
+% messenger(A) will send the messages as defined within autograder A.
+%
+%%% Remarks
+%
+% messenger is used to communicate along user-defined channels.
+%
+%%% Exceptions
+%
+% If any single method fails, then that exception is not caught.
+%
