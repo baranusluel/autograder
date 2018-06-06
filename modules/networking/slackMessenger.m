3e179b6aa44a24be5536bcde3713367f7bdc1987
Hannah White <hwhite35@gatech.edu>
2018-06-06 19:46:42 +0100

    Add formatting options to spec

--- modules/networking/slackMessenger.m
+++ modules/networking/slackMessenger.m
@@ -9,6 +9,31 @@
 %
 %%% Remarks
 %
+% M can be formatted using slack message formatting
+% ? to print bolded text, surround in asterisks. (*bold text*)
+% ? to print italicized text, surround in underscores. (_italicized text_)
+% ? to print code blocks within text, surround in single backtick marks
+% (`codeblock`)
+% See
+% https://get.slack.help/hc/en-us/articles/202288908-Format-your-messages
+% for more message formatting options
+%
 % As of now, slackMessenger will have the ability to send basic messages
 % with file attachments. Future updates will attempt to implement more
-% formatting and interactive messaging options. 
+% formatting and interactive messaging options.
+%
+
+function slackMessenger(channel,message,token,attachments)
+
+
+end
+
+
+
+
+
+
+
+
+
+
