1d5b1a4792f8b3729068a9d8c6fea30bf5c525ab
Alexander Rao <arao81@gatech.edu>
2018-06-09 17:51:39 -0500

    Add notification for desktop to messenger

--- modules/networking/messenger.m
+++ modules/networking/messenger.m
@@ -53,5 +53,27 @@ function messenger(app, students)
         slackMessenger(app.slackChannel, message, app.slackToken, [tmp filesep 'grades.csv']);
     end
     delete([tmp filesep 'grades.csv']);
+    
+    % send notification
+    javaaddpath([fileparts(mfilename('fullpath')) filesep 'JCommunique.jar']);
+    cleaner = onCleanup(@()(javarmpath([fileparts(mfilename('fullpath')) ...
+        filesep 'JCommunique.jar'])));
+    import com.notification.NotificationFactory;
+    import com.notification.NotificationManager;
+    import com.notification.manager.SimpleManager;
+    import com.notification.types.TextNotification;
+    import com.theme.ThemePackagePresets;
+    import com.utils.Time;
+    import javax.swing.ImageIcon;
+    
+    p = [fileparts(fileparts(mfilename('fullpath'))) ...
+        'docs' filesep 'resources' filesep 'images' filesep 'icon_48.png'];
+    img = ImageIcon(p);
+    
+    factory = NotificationFactory(ThemePackagePresets.cleanDark());
+    manager = SimpleManager();
+    notification = factory.buildIconNotification('Autograder', 'Autograder Finished!', img);
+    notification.setCloseOnClick(true);
+    manager.addNotification(notification, Time.seconds(100));
     [~] = rmdir(tmp, 's');
 end
