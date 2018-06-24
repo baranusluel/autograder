513bb4fd46001a789c49f7182857e28ef62d90e8
Alexander Rao <arao81@gatech.edu>
2018-06-24 12:15:07 -0500

    Add feedback emailer to autograder

--- modules/networking/emailFeedback.m
+++ modules/networking/emailFeedback.m
@@ -1,16 +1,24 @@
 %% emailFeedback: Emails feedback to students
 %
-% emailFeedback(T, K, I, C, S, M) will send message and feedback files tied 
-% to students in Student array S, using token T, key K, client ID I, and 
-% secret C.
+% emailFeedback(T, K, I, C, S, M, P) will send message and feedback files 
+% tied  to students in Student array S, using token T, key K, client ID I, 
+% and secret C. Additionally, it will update progress bar P.
 %
 %%% Remarks
 %
 % This is used instead of (or in addition to) uploading the files
-function emailFeedback(token, key, id, secret, students, message)
+function emailFeedback(token, key, id, secret, students, message, progress)
     % for each student, email feedback
-    parfor s = students(1:end)
-        emailMessenger([s.id '@gatech.edu'], 'Feedback', message, ...
-            token, id, secret, key, [s.path filesep 'feedback.html']);
+    for s = numel(students):-1:1
+        student = students(s);
+        workers(s) = parfeval(@emailMessenger, 0, ...
+            [student.id '@gatech.edu'], 'Feedback', message, token, id, ...
+            secret, key, [student.path filesep 'feedback.html']);
+    end
+    progress.Indeterminate = 'off';
+    progress.Value = 0;
+    while ~all([workers.Read])
+        workers.fetchNext();
+        progress.Value = min([progress.Value + 1/numel(workers), 1]);
     end
 end
