c9cb395a19505b1f03e55961c7141ec8b76f7d7c
Alexander Rao <arao81@gatech.edu>
2018-11-10 09:13:47 -0500

    Add necessary methods for ismember to Point

--- modules/grader/Point.m
+++ modules/grader/Point.m
@@ -60,14 +60,43 @@ classdef Point < handle
             if nargin > 1
                 this.Marker = marker;
                 this.Color = color;
+            else
+                this.Marker = '';
+                this.Color = [0 0 0];
             end
         end
         function areEqual = equals(this,that)
             %% Equals: does the equals thing
             % you know this
+            if isempty(this)
+                areEqual = [];
+                return;
+            elseif isempty(that)
+                areEqual = false;
+                return;
+            end
+            orig = this;
+            this = reshape(this, 1, []);
+            that = reshape(that, 1, []);
+            if isscalar(that)
+                tmp(numel(this)) = that;
+                tmp(:) = that;
+                that = tmp;
+                tmp = tmp(false);
+            end
+            if isscalar(this)
+                tmp(numel(that)) = this;
+                tmp(:) = this;
+                this = tmp;
+            end
             areEqual = this.dataEquals(that) ...
-                && strcmp(this.Marker,that.Marker) ...
-                && isequal(this.Color,that.Color);
+                & strcmp({this.Marker},{that.Marker}) ...
+                & cellfun(@isequal, {this.Color}, {that.Color});
+            if isscalar(orig)
+                areEqual = reshape(areEqual, size(that));
+            else
+                areEqual = reshape(areEqual, size(orig));
+            end
         end
         function tf = ne(this, that)
             tf = ~this.equals(that);
@@ -78,10 +107,39 @@ classdef Point < handle
         function areEqual = dataEquals(this,that)
             %% dataEquals: does the equals thing
             % you know this too
-            areEqual = this.X == that.X && this.Y == that.Y ...
-                && this.Z == that.Z;
+            areEqual = [this.X] == [that.X] ...
+                & [this.Y] == [that.Y] ...
+                & [this.Z] == [that.Z];
+            areEqual = reshape(areEqual, size(this));
         end
+        
+        function [sorted, inds] = sort(points, varargin)
+            if isempty(points)
+                sorted = points;
+                inds = [];
+                return;
+            elseif isscalar(points)
+                sorted = points;
+                inds = 1;
+                return;
+            end
+            xx = reshape([points.X], [], 1);
+            yy = reshape([points.Y], [], 1);
+            zz = reshape([points.Z], [], 1);
+            colors = vertcat(points.Color);
+            markers = reshape(string({points.Marker}), [], 1);
+            tmp = compose('%0.5f %0.5f %0.5f %d %d %d %s', ...
+                [xx, yy, zz], ...
+                colors, ...
+                markers);
+            [~, inds] = sort(tmp, varargin{:});
+            sorted = points(inds);
+        end
+        
     
+    end
+    
+    methods (Static)
     end
 end
 
