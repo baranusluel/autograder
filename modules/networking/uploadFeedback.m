4305fa8579ffb94fb43d5464f5d2e3468d0c4f6e
Alexander Rao <arao81@gatech.edu>
2018-11-15 17:40:38 +0530

    Use Canvas ID in uploading feedback

--- modules/networking/uploadFeedback.m
+++ modules/networking/uploadFeedback.m
@@ -43,16 +43,7 @@ function uploadFeedback(students, courseId, assignmentId, token, progress)
     progress.Message = 'Uploading Student Feedback to Canvas';
     progress.Indeterminate = 'off';
     progress.Value = 0;
-    % set up web options
-    apiOpts = weboptions;
-    apiOpts.RequestMethod = 'GET';
-    apiOpts.HeaderFields = {'Authorization', ['Bearer ' token]};
-
-    % for each student, get student ID from GT Username. Then, using id, upload grades.
-    getApiOpts = apiOpts;
-    getApiOpts.RequestMethod = 'GET';
-    putApiOpts = apiOpts;
-    putApiOpts.RequestMethod = 'PUT';
+    
     for s = numel(students):-1:1
         stud.name = students(s).name;
         stud.grade = students(s).grade;
@@ -75,78 +66,71 @@ end
 
 function uploadFile(courseId, assignmentId, student, token)
     apiOpts = weboptions;
-    apiOpts.RequestMethod = 'GET';
+    apiOpts.RequestMethod = 'POST';
     apiOpts.HeaderFields = {'Authorization', ['Bearer ' token]};
     API = 'https://gatech.instructure.com/api/v1/';
-    % for each student, get student ID from GT Username. Then, using id, upload grades.
     getApiOpts = apiOpts;
     getApiOpts.RequestMethod = 'GET';
     putApiOpts = apiOpts;
     putApiOpts.RequestMethod = 'PUT';
-    id = coveredRead([API 'courses/' courseId '/users'], getApiOpts, 'search_term', student.name);
-    if ~isempty(id)
-        id = num2str(id.id);
-        apiOpts = weboptions;
-        apiOpts.RequestMethod = 'POST';
-        apiOpts.HeaderFields = {'Authorization', ['Bearer ' token]};
-        COMMENT_API = sprintf('https://gatech.instructure.com/api/v1/courses/%s/assignments/%s/submissions/%s/comments/files', ...
-            courseId, assignmentId, id);
+    id = student.id;
+    COMMENT_API = sprintf('https://gatech.instructure.com/api/v1/courses/%s/assignments/%s/submissions/%s/comments/files', ...
+        courseId, assignmentId, id);
 
-        % Steps:
-        %   1. Get the URL to upload to. To do this, we post to COMMENT_API
-        %   with:
-        %       * name
-        %       * size
-        %       * content_type=text/html
-        %   From this, you'll get back a URL to upload the actual file to.
-        %   2. Upload the file. Get back a URL and File ID
-        %   3. Create new submission comment and add the File ID from 2.
-        
-        %% Step 1
-        % first get size
-        fileSize = dir([student.path filesep 'feedback.html']);
-        fileSize = fileSize.bytes;
-        % name is just feedback.html
-        resp = webwrite(COMMENT_API, 'name', 'feedback.html', ...
-            'size', num2str(fileSize), ...
-            'content_type', 'text/html', apiOpts);
-        uploadUrl = resp.upload_url;
-        params = resp.upload_params;
-        
-        %% Step 2
-        names = fieldnames(params);
-        uploadParams = cell(1, 2 * numel(names));
-        uploadParams(1:2:end) = names;
-        for i = 1:numel(names)
-            uploadParams{2 * i} = params.(names{i});
-        end
-        uploadParams = cellfun(@string, uploadParams, 'uni', false);
+    % Steps:
+    %   1. Get the URL to upload to. To do this, we post to COMMENT_API
+    %   with:
+    %       * name
+    %       * size
+    %       * content_type=text/html
+    %   From this, you'll get back a URL to upload the actual file to.
+    %   2. Upload the file. Get back a URL and File ID
+    %   3. Create new submission comment and add the File ID from 2.
 
+    %% Step 1
+    % first get size
+    fileSize = dir([student.path filesep 'feedback.html']);
+    fileSize = fileSize.bytes;
+    % name is just feedback.html
+    resp = webwrite(COMMENT_API, 'name', 'feedback.html', ...
+        'size', num2str(fileSize), ...
+        'content_type', 'text/html', apiOpts);
+    uploadUrl = resp.upload_url;
+    params = resp.upload_params;
 
-        request = matlab.net.http.RequestMessage;
-
-        auth = matlab.net.http.HeaderField;
-        auth.Name = 'Authorization';
-        auth.Value = ['Bearer ' token];
-
-        contentType = matlab.net.http.HeaderField;
-        contentType.Name = 'Content-Type';
-        contentType.Value = 'multipart/form-data';
-
-        request.Method = 'POST';
-        request.Header = [auth contentType];
-
-        fileProvider = matlab.net.http.io.FileProvider([student.path filesep 'feedback.html']);
-        fileProvider = matlab.net.http.io.MultipartFormProvider(uploadParams{:}, "file", fileProvider);
-        
-        request.Body = fileProvider;
-        resp = request.send(uploadUrl);
-        
-        fileId = num2str(resp.Body.Data.id);
-        
-        %% Step 3
-        coveredRead([API 'courses/' courseId '/assignments/' assignmentId '/submissions/' id], putApiOpts, 'comment[file_ids][]', fileId, 'comment[text_comment]', 'Your Feedback File');
+    %% Step 2
+    names = fieldnames(params);
+    uploadParams = cell(1, 2 * numel(names));
+    uploadParams(1:2:end) = names;
+    for i = 1:numel(names)
+        uploadParams{2 * i} = params.(names{i});
     end
+    uploadParams = cellfun(@string, uploadParams, 'uni', false);
+
+
+    request = matlab.net.http.RequestMessage;
+
+    auth = matlab.net.http.HeaderField;
+    auth.Name = 'Authorization';
+    auth.Value = ['Bearer ' token];
+
+    contentType = matlab.net.http.HeaderField;
+    contentType.Name = 'Content-Type';
+    contentType.Value = 'multipart/form-data';
+
+    request.Method = 'POST';
+    request.Header = [auth contentType];
+
+    fileProvider = matlab.net.http.io.FileProvider([student.path filesep 'feedback.html']);
+    fileProvider = matlab.net.http.io.MultipartFormProvider(uploadParams{:}, "file", fileProvider);
+
+    request.Body = fileProvider;
+    resp = request.send(uploadUrl);
+
+    fileId = num2str(resp.Body.Data.id);
+
+    %% Step 3
+    coveredRead([API 'courses/' courseId '/assignments/' assignmentId '/submissions/' id], putApiOpts, 'comment[file_ids][]', fileId, 'comment[text_comment]', 'Your Feedback File');
 end
 
 function out = coveredRead(url, opts, varargin)
