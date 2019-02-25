3efc911f09939a90efef5a16b65b7ffbc614fc6a
Alexander Rao <arao81@gatech.edu>
2019-02-25 01:53:28 -0500

    Add start of scope conversion

---
+++ modules/dataConversion/scope2autograder.m
@@ -0,0 +1,10 @@
+%% scope2autograder: Convert Gradescope submissions to Autograder format.
+%
+% scope2autograder will convert a submission download from Gradescope into
+% a format that the autograder can read.
+%
+% scope2autograder(S, P) will use submission zip path S and
+%
+%%% Remarks
+%
+% Using gradescope means that you are actively choosing not to
