6bdcc005901c600e2cb512c878deb69dc30ebe99
Alexander Rao <arao81@gatech.edu>
2019-02-25 01:47:02 -0500

    Make sure runner respects new break mode rules

--- modules/runner/autograder.m
+++ modules/runner/autograder.m
@@ -777,11 +777,8 @@ end
 
 function shouldDebug = debugger(app, msg)
     EMAIL_MESSAGE_FORMAT = 'Hello,\n\nIt appears the autograder failed to finish. Here''s the error message:\n\n%s\n\nBest Regards,\n~The CS 1371 Technology Team';
-
-    % If we are leaky (or there is a delay), don't stop
-    % If we are leaky (app.isLeaky = true) OR we have a delay
-    % (~isempty(app.delay)), don't debug
-    shouldDebug = ~(app.IsLeaky.Value || ~isempty(app.delay));
+    % If EnterBreakMode is true, we debug
+    shouldDebug = app.shouldDebug;
     % notify
     try
         if ~isempty(app.email)
